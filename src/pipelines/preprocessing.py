0eeb1c60988d8e7e5065ec08df2ffe2ef57295c8
khoonseng <khoonseng87@gmail.com>
2026-04-02 15:08:35 +0800

    add pca to lightgbm

--- src/pipelines/preprocessing.py
+++ src/pipelines/preprocessing.py
@@ -2,6 +2,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.decomposition import PCA
 
 def convert_bool_to_int(df):
     # Ensure we are working on a copy to avoid SettingWithCopy warnings
@@ -53,3 +54,27 @@ def build_tree_preprocessor(num_features, cat_features=None):
         ])
     
     return preprocessor
+
+def build_pca_preprocessor(num_features, cat_features, n_components=0.95):
+    numerical_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler()),
+        ("pca", PCA(n_components=n_components))
+    ])
+
+    categorical_pipeline = Pipeline([
+        ('to_int', FunctionTransformer(func=convert_bool_to_int, feature_names_out="one-to-one")),
+        ("imputer", SimpleImputer(strategy="most_frequent"))
+    ])
+
+    if cat_features is not None:
+        preprocessor = ColumnTransformer([
+            ("num", numerical_pipeline, num_features),
+            ("cat", categorical_pipeline, cat_features)
+        ])
+    else:
+        preprocessor = ColumnTransformer([
+            ("num", numerical_pipeline, num_features)
+        ])
+
+    return preprocessor    
